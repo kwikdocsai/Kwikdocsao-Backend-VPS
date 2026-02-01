81f3cf518d3d7c9b7b95f76f72896b6629f04cd9
conversioao <conversioao@gmail.com>
2026-02-01 20:12:07 +0000

    4

--- dist/run-fiscal-migration.js
+++
@@ -1,4 +0,0 @@
-import { runAddFiscalColumnsToCompaniesMigration } from './migrations/31_add_fiscal_columns_to_companies.js';
-(async () => {
-    await runAddFiscalColumnsToCompaniesMigration();
-})();
